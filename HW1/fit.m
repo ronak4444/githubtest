8eae0ed1f98a8ca8205638ef1f12b5daf9a69bc8
ronak <learnronak@outlook.com>
2014-03-16 19:15:14 +0530

    delete action sync test
    deleted a portion of fit.m for checking how github syncs it.

--- HW1/fit.m
+++ HW1/fit.m
@@ -11,22 +11,5 @@ global      popsize;
 global      length;
 
 
-%-------------------------------------------bit counter
-
-fitness=    zeros(popsize,1);
-
-for     i=  1:popsize
-        t= 0;
-    for j=  1:length
-        
-        if  pop(i,j)==  1
-            t=t+1;
-        else
-           
-        end
-    end
-    fitness(i,1)= t;
-end
-
 
 
